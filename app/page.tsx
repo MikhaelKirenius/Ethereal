850d5098d043ab6d122a80006d9a9a468937abc3
MikhaelKirenius <92681258+MikhaelKirenius@users.noreply.github.com>
2025-02-07 20:58:59 +0700

    change background

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import HeroSection from "./components/HeroSection"
-import InteractiveWaveGradient from "./components/Dynamic";
 import FeaturesSection from "./components/FeaturesSection";
 import Navbar from "./components/Navbar";
 import CTASection from "./components/CtaSection";
@@ -8,23 +7,22 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
-    <InteractiveWaveGradient />
-    <div className="relative z-10">
-      <Navbar />
-      <div className="flex flex-col">
-        <div id="hero">
-          <HeroSection />
+    <div className="relative min-h-screen bg-gradient-to-br from-purple-600/40 via-blue-600/40 to-teal-600/40">
+      <div className="relative z-10">
+        <Navbar />
+        <div className="flex flex-col">
+          <div id="hero" className="bg-white">
+            <HeroSection />
+          </div>
+          <div id="features">
+            <FeaturesSection />
+          </div>
+          <div id="cta">
+            <CTASection />
+          </div>
+          <Footer />
         </div>
-        <div id="features">
-          <FeaturesSection />
-        </div>
-        <div id="cta">
-          <CTASection />
-        </div>
-        <Footer />
       </div>
     </div>
-  </div>
   );
 }
